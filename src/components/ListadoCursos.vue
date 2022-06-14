b14d14db17374d21c52c77f516036501fa13b46b
Abril Suarez <abril.suarez@algardata.pt>
2022-06-13 21:25:47 -0300

    primer commit <3

---
+++ src/components/ListadoCursos.vue
@@ -0,0 +1,42 @@
+<template>
+    <base-cursos>
+        <h1>Mis cursos</h1>
+        <ion-list>
+          <!-- <ion-item>holaa</ion-item>
+          <ion-item>chauu</ion-item> -->
+          <ion-item :key="curso.id" v-for="curso in cursos">
+            <ion-label>
+              <strong>{{curso.nombre}}</strong> <br>
+              <p>Cant. de estudiantes: {{curso.estudiantes}}</p>
+            </ion-label>
+          </ion-item>
+
+        </ion-list>
+    </base-cursos>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { IonList, IonItem, IonLabel } from '@ionic/vue';
+import Tab2Page from '../views/Tab2Page.vue';
+
+export default ({
+  name: 'ListadoCursos',
+  // props: {
+  //   name: String
+  // },
+  components: {
+    IonList,
+    IonItem,
+    IonLabel,
+    Tab2Page
+  },
+  data() { 
+    return {
+      cursos: [ { id: 1, nombre: "historia", estudiantes: 6}, 
+                { id: 2, nombre: "inundaciones", estudiantes: 10}, 
+                { id: 3, nombre: "arroyo maldonado", estudiantes: 4}
+              ] 
+    }}
+});
+</script>
