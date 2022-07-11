72735aa3c97e9abbf9fa2d42a5eb98b1f7bd3042
Ignacio Cazorla <nacla12@hotmail.com>
2022-07-11 11:51:57 -0300

    Agrega vista de bienvenida y login.

---
+++ ionic-app/src/views/WelcomePage.vue
@@ -0,0 +1,38 @@
+<template>
+    <ion-page>
+        <ion-header>
+        </ion-header>
+        <ion-content fullscreen="true">
+            <div id="container">
+                <h1> NombreDeLaApp </h1>
+                <p> NombreDeLaApp te permite gestionar y acceder tus materiales educativos. 
+                    Ádemas es una herramienta que permite enseñar y aprender de una manera distinta.
+                </p>
+                <ion-button router-link="login"> Comenzar </ion-button>
+            </div>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonContent, IonButton } from "@ionic/vue";
+
+export default {
+    components:{
+        IonPage,
+        IonHeader,
+        IonContent,
+        IonButton
+    }    
+}
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+}
+
+</style>
